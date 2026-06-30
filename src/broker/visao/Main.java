6386622b6a1c9562b342798b74ad5d60f3688528
matheusoare <matheussoares_ramos@outlook.com>
2026-06-29 23:19:56 -0300

    Features : Retirando pasta src duplicada

--- src/broker/visao/Main.java
+++
@@ -1,9 +0,0 @@
-package broker.visao;
-
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new JanelaPrincipal().setVisible(true));
-    }
-}
